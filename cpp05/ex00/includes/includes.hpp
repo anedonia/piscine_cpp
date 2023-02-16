7845c8b7f5517ecde0ae6d180768f2e8480ba7a8
anedonia <deuxvys@gmail.com>
2023-02-16 21:48:14 +0100

    archi day 5

---
+++ cpp05/ex00/includes/includes.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 14:04:28 by ldevy             #+#    #+#             */
+/*   Updated: 2023/02/14 15:09:53 by ldevy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDES_HPP
+#define INCLUDES_HPP
+
+#include <string>
+#include <iostream>
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+#endif

40f4e690f9cc9a501a6772beb691ff223f56c829
anedonia <deuxvys@gmail.com>
2023-02-21 18:03:50 +0100

    fatigue

--- cpp05/ex01/includes/Form.hpp
+++ cpp05/ex01/includes/Form.hpp
@@ -1,20 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   includes.hpp                                       :+:      :+:    :+:   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/14 14:04:28 by ldevy             #+#    #+#             */
-/*   Updated: 2023/02/21 16:29:36 by ldevy            ###   ########.fr       */
+/*   Created: 2023/02/21 17:58:49 by ldevy             #+#    #+#             */
+/*   Updated: 2023/02/21 18:00:11 by ldevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INCLUDES_HPP
-#define INCLUDES_HPP
+#ifndef FORM_HPP
+#define FORM_HPP
 
-#include <string>
-#include <iostream>
-#include "Bureaucrate.hpp"
+class Form
+{
+private:
+    /* data */
+public:
+    Form();
+    Form();
+    Form();
+    ~Form();
+};
+
+Form::Form(/* args */)
+{
+}
+
+Form::~Form()
+{
+}
 
 #endif
--- cpp05/ex01/srcs/Form.cpp
+++ cpp05/ex01/srcs/Form.cpp
@@ -1,20 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   includes.hpp                                       :+:      :+:    :+:   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/14 14:04:28 by ldevy             #+#    #+#             */
-/*   Updated: 2023/02/21 16:29:36 by ldevy            ###   ########.fr       */
+/*   Created: 2023/02/21 17:57:45 by ldevy             #+#    #+#             */
+/*   Updated: 2023/02/21 17:58:21 by ldevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INCLUDES_HPP
-#define INCLUDES_HPP
-
-#include <string>
-#include <iostream>
-#include "Bureaucrate.hpp"
-
-#endif
+#include "../includes/includes.hpp"

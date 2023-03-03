140ec54501a4560ec102582f48b0cf5a5a92c1bc
anedonia <deuxvys@gmail.com>
2023-03-03 14:44:48 +0100

    yeay fix cpp05 ex01

--- cpp05/ex01/includes/Form.hpp
+++ cpp05/ex01/includes/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 17:58:49 by ldevy             #+#    #+#             */
-/*   Updated: 2023/03/03 14:35:49 by ldevy            ###   ########.fr       */
+/*   Updated: 2023/03/03 14:44:21 by ldevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,60 @@ class Bureaucrate;
 class Form
 {
 private:
-    /* data */
+	const std::string _name;
+	const std::string _target;
+	bool _isSigned;
+	const unsigned int _gradeToSign;
+	const unsigned int _gradeToExecute;
+	Form();
+	
 public:
-    Form();
-    Form();
-    Form();
-    ~Form();
+
+    Form(std::string name, std::string target, unsigned int signg, unsigned int execg);
+    Form(const Form & inst);
+    virtual ~Form();
+
+	Form & operator=(const Form & inst);
+
+	void			beSigned(const Bureaucrate & inst);
+	unsigned int	getToSign() const;
+	unsigned int	getToExecute() const;
+	bool			getIsSigned() const;
+	std::string		getName() const;
+	std::string		getTarget() const;
+
+	class GradeTooHighException : public std::exception
+	{
+
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("Form : Grade is too high exception");
+		}
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("Form : Grade is too low exception");
+		}
+	};
+	class FormNotSignedException : public std::exception
+	{
+		
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("Form : Isn't signed :/");
+		}
+	};
 };
 
-Form::Form(/* args */)
-{
-}
+std::ostream & operator<<(std::ostream& o, const Form & inst);
 
-Form::~Form()
-{
-}
+
 
 #endif
